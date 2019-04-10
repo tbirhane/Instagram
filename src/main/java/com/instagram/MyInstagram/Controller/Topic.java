f2139fceab05605b9507dcf4ba3599352e25b788
tbirhane <tbirhane@mum.edu.com>
2019-04-09 23:55:48 -0500

    modified

--- src/main/java/com/instagram/MyInstagram/Controller/Topic.java
+++ src/main/java/com/instagram/MyInstagram/Controller/Topic.java
@@ -4,7 +4,7 @@ public class Topic {
 	private String id;
 	private String name;
 	private String description;
-	
+	//Empty constructor
 	public Topic() {
 	}
 	public Topic(String id, String name, String description) {
